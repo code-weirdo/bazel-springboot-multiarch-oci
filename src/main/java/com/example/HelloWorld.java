3bf2645b1866957f642bff36fe6e1ab8668da0c6
Nicholas Prozesky <nicholas.prozesky@vitalitygroup.com>
2024-12-22 21:51:51 -0300

    bootified hello world
    - the application is now a spring boot application
    - multi-arch breaks the springboot rule

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,8 +1,22 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SpringApplication.run(HelloWorld.class, args);
+    }
+
+    @Component
+    static class GreeterComponent implements CommandLineRunner {
+        @Override
+        public void run(String... args) {
+            System.out.println("Hello World!");
+        }
     }
 }
